e7c432a1ce62f00bba077cdc5e5502cc70303371
fubispb <fubispb@yandex.ru>
2020-11-02 13:22:49 +0300

    Rewrite multithreading code with Streams and Lambdas

--- src/main/java/Multithreading/Test.java
+++ src/main/java/Multithreading/Test.java
@@ -1,72 +1,34 @@
 package Multithreading;
 
 
-import java.util.concurrent.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 
 public class Test {
 
     private static Wrapper wrapper;
+    ExecutorService fixedPool = Executors.newFixedThreadPool(4, Thread::new);
+    ExecutorService singleExecutor = Executors.newSingleThreadExecutor(Thread::new);
+    ExecutorService cachedPool = Executors.newCachedThreadPool(Thread::new);
+    List<ExecutorService> executors = Arrays.asList(fixedPool, singleExecutor, cachedPool);
 
 
     public static void main(String[] args) {
+        Test test = new Test();
         wrapper = new Wrapper();
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                wrapper.increment();
-            }
-        });
-        ExecutorService fixedPool = Executors.newFixedThreadPool(4, new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                return new Thread(r);
-            }
-        });
-
-        ExecutorService singleExecutor = Executors.newSingleThreadExecutor(new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                return new Thread(r);
-            }
-        });
-
-        ExecutorService cachedPool = Executors.newCachedThreadPool(new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                return new Thread(r);
-            }
-        });
+        test.executorsCallIncrement();
+        Thread t1 = new Thread(wrapper::increment);
         t1.start();
-        for (int i = 0; i < 5; i++) {
-            fixedPool.execute(new Runnable() {
-                @Override
-                public void run() {
-                    wrapper.increment();
-                }
-            });
-        }
-        singleExecutor.execute(new Runnable() {
-            @Override
-            public void run() {
-                wrapper.increment();
-            }
-        });
-        for (int i = 0; i < 4; i++) {
-            cachedPool.execute(new Runnable() {
-                @Override
-                public void run() {
-                    wrapper.increment();
-                }
-            });
-        }
+    }
+
+    public void executorsCallIncrement() {
+        executors.stream().flatMapToInt(executorService -> IntStream.range(0, 5)).forEach(i -> wrapper.increment());
         fixedPool.shutdown();
         singleExecutor.shutdown();
         cachedPool.shutdown();
-
     }
-
 }
-
-
-
--- src/main/java/Multithreading/Wrapper.java
+++ src/main/java/Multithreading/Wrapper.java
@@ -2,21 +2,25 @@ package Multithreading;
 
 import lombok.Data;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Data
 public class Wrapper {
 
-    public static volatile int commonInt = 0;
+    public final AtomicInteger commonInt = new AtomicInteger(0);
 
-    public synchronized void increment() {
-        long start = System.currentTimeMillis();
-        System.out.println(Thread.currentThread().getName() + " entered Wrapper increment() method at " + start);
-        System.out.println("commonInt before is: " + commonInt);
-        for (int i = 0; i < 100000; i++) {
-            commonInt++;
+    public void increment() {
+        synchronized (commonInt) {
+            long start = System.currentTimeMillis();
+            System.out.println(Thread.currentThread().getName() + " entered Wrapper increment() method at " + start);
+            System.out.println("commonInt before is: " + commonInt);
+            for (int i = 0; i < 100000; i++) {
+                commonInt.getAndIncrement();
+            }
+            System.out.println("commonInt after is: " + commonInt);
+            System.out.println(Thread.currentThread().getName() + " exit Wrapper increment() method at " +
+                    System.currentTimeMillis() + " spend " + (System.currentTimeMillis() - start) + "ms");
         }
-        System.out.println("commonInt after is: " + commonInt);
-        System.out.println(Thread.currentThread().getName() + " exit Wrapper increment() method at " +
-                System.currentTimeMillis() + " spend " + (System.currentTimeMillis() - start) + "ms");
     }
 
 
